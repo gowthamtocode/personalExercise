0f2bb5a6002e36183b13113f11c03fc2be31832e
maintwizdev5 <maintwizdev5@maintwizdev-gowtham>
2021-12-09 13:19:32 +0530

    check

--- maintwiz/src/main/java/com/tpm/maintwiz/MaintwizApplication.java
+++ maintwiz/src/main/java/com/tpm/maintwiz/MaintwizApplication.java
@@ -2,7 +2,7 @@ package com.tpm.maintwiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class MaintwizApplication {
 
