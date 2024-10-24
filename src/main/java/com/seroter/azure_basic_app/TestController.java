50ee25bc264b5d5aeea0fa5d38b6e8888f9143a3
Gowtham Ramachandran <gowthamram202@gmail.com>
2024-10-24 21:06:55 +0530

    Security and Application context

--- src/main/java/com/seroter/azure_basic_app/TestController.java
+++ src/main/java/com/seroter/azure_basic_app/TestController.java
@@ -1,11 +1,10 @@
 package com.seroter.azure_basic_app;
 
+
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class TestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${institute.name}")
+    private String instituteName;
+
     @GetMapping(value = "/testApi")
     public Response doStuff(@RequestParam("check") String check){
         log.info("received string - {} ",check);
@@ -26,6 +32,11 @@ public class TestController {
     @GetMapping(value = "/workout")
     public String workoutSession(){
         return "Hey its done!!";
+    }
+
+    @GetMapping(value = "/teamInfo")
+    public String teamInfo(){
+        return coachName + " - " + instituteName;
     }
 
     @GetMapping(value = "/session")
